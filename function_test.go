f22bb9f2978ecd3eec5dcdbfbba8d0e11e081016
Steve Traugott <stevegt@t7a.org>
2021-01-07 15:50:29 -0800

    add TestGet, add err checks

--- function_test.go
+++ function_test.go
@@ -6,15 +6,70 @@ import (
 	"testing"
 )
 
+const dir = "var"
+
 func TestPut(t *testing.T) {
-	db := Open("var")
+	db, err := Open(dir)
+	if err != nil {
+		t.Fatal(err)
+	}
+	key := []byte("somekey")
 	val := []byte("somevalue")
-	db.Put([]byte("somekey"), val)
+	err = db.Put(key, val)
+	if err != nil {
+		t.Fatal(err)
+	}
 	buf, err := ioutil.ReadFile("var/somekey")
 	if err != nil {
 		t.Fatal(err)
 	}
 	if bytes.Compare(buf, val) != 0 {
-		t.Fatal("expected somevalue, got", buf)
+		t.Fatalf("expected %s, got %s", string(val), string(buf))
 	}
 }
+
+func TestGet(t *testing.T) {
+	db, err := Open(dir)
+	if err != nil {
+		t.Fatal(err)
+	}
+	key := []byte("somekey")
+	val := []byte("somevalue")
+	err = db.Put(key, val)
+	if err != nil {
+		t.Fatal(err)
+	}
+	got, err := db.Get(key)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if bytes.Compare(got, val) != 0 {
+		t.Fatalf("expected %s, got %s", string(val), string(got))
+	}
+}
+
+/*
+func TestLock(t *testing.T) {
+	db, err := Open(dir)
+	if err != nil {
+		t.Fatal(err)
+	}
+	key := []byte("somekey")
+	val := []byte("somevalue")
+
+	go func() {
+
+	}
+
+
+
+	db.Put(key, val)
+	got, err := db.Get(key)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if bytes.Compare(got, val) != 0 {
+		t.Fatalf("expected %s, got %s", string(val), string(got))
+	}
+}
+*/
