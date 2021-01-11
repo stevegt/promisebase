5a332e831b8e7d60f62598f5fefca7e38fd2c7c7
Steve Traugott <stevegt@t7a.org>
2021-01-11 15:03:55 -0800

    move ExLock() and UnLock() from skeleton.go to pitbase.go

--- pitbase.go
+++ pitbase.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"io/ioutil"
 	"os"
+	"syscall"
 )
 
 // Db is a key-value database
@@ -38,5 +39,24 @@ func (db Db) Get(key []byte) (val []byte, err error) {
 	if err != nil {
 		return val, err
 	}
+	return
+}
+
+// ExLock uses syscall.Flock to get an exclusive lock (LOCK_EX) on the
+// file referenced by `key`.
+func (db *Db) ExLock(key []byte) (fd uintptr, err error) {
+	fh, err := os.Open(fmt.Sprintf("%s/%s", db.Dir, string(key)))
+	if err != nil {
+		return
+	}
+	fd = fh.Fd()
+	syscall.Flock(int(fd), syscall.LOCK_EX)
+	return
+}
+
+// Unlock uses syscall.Flock to unlock (LOCK_UN) the file referenced
+// by `key`.
+func (db *Db) Unlock(fd uintptr) (err error) {
+	syscall.Flock(int(fd), syscall.LOCK_UN)
 	return
 }
