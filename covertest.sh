19958882adcf8b1e62a484bebe0a20c548f0e0ad
Steve Traugott <stevegt@t7a.org>
2021-01-20 14:39:40 -0800

    add coverage html output, move things from skeleton to pitbase

--- covertest.sh
+++ covertest.sh
@@ -1,11 +1,15 @@
 #!/bin/bash 
 
 minpct=80
-cmd="go test -v -timeout 10s -cover ./..."
+cmd="go test -v -timeout 10s -cover -coverprofile=/tmp/covertest.out -coverpkg=./..."
 tmp=/tmp/$$
 
 $cmd 2>&1 | tee $tmp  
-pct=$(cat $tmp | tail -1 | perl -ne 'print if s/.*coverage:\s+(\d+)\..*/$1/m')
+go tool cover -html=/tmp/covertest.out -o /tmp/covertest.html
+echo run this to see coverage detail: 
+echo xdg-open /tmp/covertest.html
+
+pct=$(cat $tmp | grep coverage: | tail -1 | perl -ne 'print if s/.*coverage:\s+(\d+)\..*/$1/')
 if test "$pct" -le "$minpct"  
 then
 	echo FAIL coverage less than $minpct
