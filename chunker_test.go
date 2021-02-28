370828002db0c287d89713a5f7586ef5c79c0139
Steve Traugott <stevegt@t7a.org>
2021-02-28 12:16:31 -0800

    get TestChunker() and TestPutStream() working
    - write Stream as io.Reader
    - PutStream() still needs implementation

--- chunker_test.go
+++ chunker_test.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"fmt"
 	"io"
+	"math"
 	"math/rand"
 	"testing"
 
@@ -19,19 +20,11 @@ func TestChunker(t *testing.T) {
 	tassert(t, chunker.Poly > 0, "polynomial is %v", chunker.Poly)
 
 	// create some data
-	size := 10 * miB
-	var stream []byte
-	stream = make([]byte, size)
-	rand.Seed(42)
-	// write random data into stream buf
-	n, err := rand.Read(stream)
-	tassert(t, err == nil, "rand.Read(): %v", err)
-	tassert(t, size == n, "size: expected %d got %d", size, n)
-	tassert(t, size == len(stream), "size: expected %d got %d", size, n)
+	size := 100 * miB
+	stream := genstream(t, size)
 
 	// chunk it
-	rd := bytes.NewReader(stream)
-	chunker.Start(rd)
+	chunker.Start(stream)
 
 	buf := make([]byte, 1*miB)
 	var gotstream []byte
@@ -42,26 +35,57 @@ func TestChunker(t *testing.T) {
 			break
 		}
 		fmt.Printf(".")
-		expect := stream[chunk.Start : chunk.Start+chunk.Length]
+		expect := stream.Data[chunk.Start : chunk.Start+chunk.Length]
 		tassert(t, bytes.Compare(expect, chunk.Data) == 0, "chunk: expected %v got %v", expect, chunk.Data)
 		gotstream = append(gotstream, chunk.Data...)
 	}
 	gotsize := len(gotstream)
 	tassert(t, size == int(gotsize), "size: expected %d got %d", size, gotsize)
-	tassert(t, bytes.Compare(stream, gotstream) == 0, "chunk: stream vs. gotstream mismatch")
+	tassert(t, bytes.Compare(stream.Data, gotstream) == 0, "chunk: stream vs. gotstream mismatch")
+}
+
+type Stream struct {
+	Data    []byte
+	nextPos int64
+}
+
+func (s *Stream) Read(p []byte) (n int, err error) {
+	bs := 4096 // XXX try different sizes
+	start := s.nextPos
+	if start >= int64(len(s.Data)) {
+		err = io.EOF
+		return
+	}
+	end := int64(math.Min(float64(start)+float64(bs), float64(len(s.Data))))
+	n = copy(p, s.Data[start:end])
+	s.nextPos = end
+	return
+}
+
+func genstream(t *testing.T, size int) (stream *Stream) {
+	stream = &Stream{}
+	stream.Data = make([]byte, size)
+	rand.Seed(42)
+	// write random data into stream.Data
+	n, err := rand.Read(stream.Data)
+	tassert(t, err == nil, "rand.Read(): %v", err)
+	tassert(t, size == n, "size: expected %d got %d", size, n)
+	tassert(t, size == len(stream.Data), "size: expected %d got %d", size, n)
+	// t.Fatal("sadf")
+	return
 }
 
 func TestPutStream(t *testing.T) {
-	// db := newdb()
-	/*
-		db, err := Open(dir)
-		if err != nil {
-			t.Fatal(err)
-		}
+	stream := genstream(t, 100*miB)
+	db := newdb(t)
 
+	node, err := db.PutStream(stream)
+	tassert(t, err == nil, "PutStream(): %v", err)
+	tassert(t, node != nil, "PutStream() node is nil")
 
+	gotbuf, err := node.Cat()
+	tassert(t, err == nil, "node.Cat(): %v", err)
 
-		stream := ioutil.
-		// chunker =
-	*/
+	tassert(t, bytes.Compare(stream.Data, *gotbuf) == 0, "chunk: stream vs. gotbuf mismatch")
+
 }
