d5670254e9b418d38d71f96c48a7e420f02d143b
Steve Traugott <stevegt@t7a.org>
2021-03-25 16:30:15 -0700

    doc.go: clarify node, stream, and label

--- doc.go
+++ doc.go
@@ -15,10 +15,12 @@ Vocabulary:
 	in order to keep directory sizes small; the number of subdirs is fixed
 	at database creation
 - blob: chunk or block of data; deduplication atom; stored as file
-- node: list of one or more blobs or nodes; stored as blob or node canpaths
+- node: list of one or more blobs or nodes; stored as file containing blob or node canpaths
 - rootnode: the top-level node for a stream
-- stream: ordered set of one or more blobs; stored as rootnode canpath
-- label: human-readable name of a stream; stored as symlink pointing at rootnode canpath
+- stream: ordered set of one or more blobs; stored as a symlink
+  pointing at rootnode canpath
+- label: human-readable name of a stream;
+  stored as the name of the symlink pointing at rootnode canpath
 - object: blob, node, or stream; addressed by canpath
 - key: in-memory representation of a blob or node location; provides
   methods for generating abspath, relpath, canpath, or hash XXX deprecate?
