aef0b84ed6e4490d2e13fa8b5b9f962cf4f60ff8
Steve Traugott <stevegt@t7a.org>
2021-03-25 17:03:11 -0700

    fix multi-value bug in objectExample()

--- function_test.go
+++ function_test.go
@@ -213,7 +213,8 @@ func TestBlob(t *testing.T) {
 // XXX this is just temporarily here as an example of how an Object
 // might be used
 func objectExample(o Object) {
-	fmt.Printf("object %s is %d bytes", o.CanPath(), o.Size())
+	size, err := o.Size()
+	fmt.Printf("object %s is %d bytes", o.CanPath(), size)
 }
 
 func TestPut(t *testing.T) {
