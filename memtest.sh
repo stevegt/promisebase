bf0787bc02cdf66574a0d0818605ac6165f65da8
Jessica Traugott <jessica@t7a.org>
2021-04-16 18:53:11 -0700

    memtest: add note about creating wide trees

--- memtest.sh
+++ memtest.sh
@@ -6,12 +6,13 @@ pb=$PWD/cmd/pb/pb
 cd cmd/pb
 go build
 
-tmpdir=/tmp/stresstest.$$
+basename=/tmp/stresstest
+tmpdir=$basename.$$
 
 # seed rng
 #RANDOM=42
 
-for i in {22..1}
+for i in {10..1}
 do
 	echo i is $i
 	ulimit -v $(( $i * 100 * 1024 )) # kilobytes
@@ -20,14 +21,21 @@ do
 	mkdir -p $tmpdir
 	cd $tmpdir
 	#size=$(( $RANDOM * $RANDOM / 1024 ))
-    size=$(( 1024 * 1024 ))
+    size=$(( 512 * 1024 ))
 
 	$pb init 
 
-    canpath=$(dd if=/dev/urandom bs=1024 count=$size | $pb putblob sha256)
-	$pb getblob $canpath > /dev/null
+    blobpath1=$(dd if=/dev/urandom bs=1024 count=$size | $pb putblob sha256)
+	$pb getblob $blobpath1 > /dev/null
 
-	# pb puttree <algo> <canpaths>... 
+    blobpath2=$(dd if=/dev/urandom bs=1024 count=$size | $pb putblob sha256)
+    treepath1=$($pb puttree sha256 $blobpath1 $blobpath2)
+
+    # for j in {1..8000000}
+    # do
+        # XXX this will not work because we cant put 8 million
+        # canpaths on the puttree command line
+
 	# pb gettree <canpath>
 	# pb linkstream <canpath> <name>
 	# pb getstream <name>
@@ -37,6 +45,7 @@ do
 	# pb putstream [-q] <algo> <name>
 
 	[ -n "$tmpdir" ]
+    echo $tmpdir | grep $basename
 	rm -rf $tmpdir
 done
 
